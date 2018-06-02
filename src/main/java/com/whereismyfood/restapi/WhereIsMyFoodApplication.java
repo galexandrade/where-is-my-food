f2811da0f15103b60c55052c3867d401cd526138
Alex P. Andrade <g.alex.andrade@gmail.com>
2018-06-02 10:11:38 -0300

    First commit

---
+++ src/main/java/com/whereismyfood/restapi/WhereIsMyFoodApplication.java
@@ -0,0 +1,12 @@
+package com.whereismyfood.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereIsMyFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereIsMyFoodApplication.class, args);
+	}
+}
