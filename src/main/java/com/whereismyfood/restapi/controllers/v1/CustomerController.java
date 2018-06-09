f20a4ec76bec2c1902ffdfbff65f8e6eed7c8f68
Alex P. Andrade <g.alex.andrade@gmail.com>
2018-06-08 21:33:50 -0300

    FIX Customer controller

--- src/main/java/com/whereismyfood/restapi/controllers/v1/CustomerController.java
+++ src/main/java/com/whereismyfood/restapi/controllers/v1/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(CustomerController.BASE_URL)
 public class CustomerController {
-    public static final String BASE_URL = "/api/v1/customer";
+    public static final String BASE_URL = "/api/v1/customers";
 
     private final CustomerService customerService;
 
